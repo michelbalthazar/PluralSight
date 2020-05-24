4ecfa14797a11a1a1c4018902c41b8c8de36851a
Michel Balthazar <michel@nfe.io>
2020-05-24 20:56:42 -0300

    Created a new controller and view to call in a new route. Test get information from web.config

---
+++ asp-net-mvc-5-fundamentals/OdeToFood/src/OdeToFood.Web/Controllers/GreetingController.cs
@@ -0,0 +1,20 @@
+﻿using OdeToFood.Web.Models;
+using System.Configuration;
+using System.Web.Mvc;
+
+namespace OdeToFood.Web.Controllers
+{
+    public class GreetingController : Controller
+    {
+        // GET: Greeting
+        public ActionResult Index()
+        {
+            var model = new GreetingViewModel
+            {
+                Message = ConfigurationManager.AppSettings["Message"]
+            };
+
+            return View(model);
+        }
+    }
+}
