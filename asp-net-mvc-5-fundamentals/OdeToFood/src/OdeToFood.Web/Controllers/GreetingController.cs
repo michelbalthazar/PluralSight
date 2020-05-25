3253304fad90c883e4fc0b808aafb77c2fecc6f8
Michel Balthazar <michel@nfe.io>
2020-05-25 16:47:50 -0300

    implement name parameter on GreetingController's index method

--- asp-net-mvc-5-fundamentals/OdeToFood/src/OdeToFood.Web/Controllers/GreetingController.cs
+++ asp-net-mvc-5-fundamentals/OdeToFood/src/OdeToFood.Web/Controllers/GreetingController.cs
@@ -7,11 +7,12 @@ namespace OdeToFood.Web.Controllers
     public class GreetingController : Controller
     {
         // GET: Greeting
-        public ActionResult Index()
+        public ActionResult Index(string name)
         {
             var model = new GreetingViewModel
             {
-                Message = ConfigurationManager.AppSettings["Message"]
+                Message = ConfigurationManager.AppSettings["Message"],
+                Name = name ?? "no name"
             };
 
             return View(model);
