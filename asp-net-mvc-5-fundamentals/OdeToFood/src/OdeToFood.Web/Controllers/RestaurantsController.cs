277cd2f048f1e7016863b997149aceaf5dfffedc
Michel Balthazar <michel@nfe.io>
2020-05-25 19:59:14 -0300

    Implemented RestaurantsController and some views about restaurant

---
+++ asp-net-mvc-5-fundamentals/OdeToFood/src/OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -0,0 +1,31 @@
+﻿using OdeToFood.Data.Services;
+using System.Web.Mvc;
+
+namespace OdeToFood.Web.Controllers
+{
+    public class RestaurantsController : Controller
+    {
+        private readonly IRestaurantData _db;
+
+        public RestaurantsController(IRestaurantData db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            var model = _db.GetAll();
+            return View(model);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var model = _db.GetOne(id);
+
+            if (model == null)
+                return View("NotFound");
+
+            return View(model);
+        }
+    }
+}
