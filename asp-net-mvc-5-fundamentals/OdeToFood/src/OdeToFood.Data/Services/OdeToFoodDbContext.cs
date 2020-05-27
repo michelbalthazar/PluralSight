cc1205928a10e53ba254f98864ff6a188813c34a
Michel Balthazar <michel@nfe.io>
2020-05-27 10:02:34 -0300

    implement sql data base context

---
+++ asp-net-mvc-5-fundamentals/OdeToFood/src/OdeToFood.Data/Services/OdeToFoodDbContext.cs
@@ -0,0 +1,15 @@
+﻿using OdeToFood.Data.Models;
+using System.Data.Entity;
+
+namespace OdeToFood.Data.Services
+{
+    public class OdeToFoodDbContext : DbContext
+    {
+        public OdeToFoodDbContext()
+        {
+
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
---
+++ asp-net-mvc-5-fundamentals/OdeToFood/src/OdeToFood.Data/Services/SqlRestaurantData.cs
@@ -0,0 +1,51 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private readonly OdeToFoodDbContext _db;
+
+        public SqlRestaurantData(OdeToFoodDbContext db)
+        {
+            _db = db;
+        }
+
+        public void Add(Restaurant restaurant)
+        {
+            _db.Restaurants.Add(restaurant);
+            _db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var entity = GetOne(id);
+
+            _db.Restaurants.Remove(entity);
+            _db.SaveChanges();
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from e in _db.Restaurants
+                   orderby e.Name
+                   select e;
+        }
+
+        public Restaurant GetOne(int id)
+        {
+            return _db.Restaurants.FirstOrDefault(e => e.Id == id);
+        }
+
+        public void Update(Restaurant restaurant)
+        {
+            var entry = _db.Entry(restaurant);
+            entry.State = EntityState.Modified;
+            _db.SaveChanges();
+        }
+    }
+}
