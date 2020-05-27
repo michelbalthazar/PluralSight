b9eb7675d9a1e34358dc60d452eb9049cb3ebb3b
Michel Balthazar <michel@nfe.io>
2020-05-27 10:01:06 -0300

    implement DataAnnotations at restaurant model

--- asp-net-mvc-5-fundamentals/OdeToFood/src/OdeToFood.Data/Models/Restaurant.cs
+++ asp-net-mvc-5-fundamentals/OdeToFood/src/OdeToFood.Data/Models/Restaurant.cs
@@ -5,11 +5,12 @@ namespace OdeToFood.Data.Models
     public class Restaurant
     {
         public int Id { get; set; }
+
         [Required]
+        [MaxLength(255)]
         public string Name { get; set; }
+
         [Display(Name = "Type of food")]
         public CuisineType Cuisine { get; set; }
     }
-
-
 }
