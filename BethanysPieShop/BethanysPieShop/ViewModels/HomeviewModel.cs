c2da238bbe7d11df4280087466fdc74f44dc80e8
Michel Balthazar <michel@cliqx.com.br>
2021-08-09 14:32:21 -0300

    refactor index and lixt view to use partial view

---
+++ BethanysPieShop/BethanysPieShop/ViewModels/HomeviewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using BethanysPieShop.Models;
+
+namespace BethanysPieShop.ViewModels
+{
+    public class HomeviewModel
+    {
+        public IEnumerable<Pie> PiesOfTheWeek { get; set; }
+    }
+}
